d1494d388b4ddf7dead487a435ebd3d758fdf50c
D7703_29 <d7703_29@d7703-29ui-imac.local>
2018-03-13 15:50:32 +0900

    버튼을 누르면 레이블이 바껴요

--- buttonfun03/ViewController.swift
+++ buttonfun03/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var count = 0
+    
+    @IBOutlet weak var La1: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.cyan
     }
 
+    @IBAction func Button(_ sender: Any) {
+        count = count + 1
+        print("\(count)Button!!")
+        La1.text = "\(count) Button"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
